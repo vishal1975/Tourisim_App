2873c757ca79a9ee5775e23dce8e10f6d202d2e0
vishal chaudhary <vishalchaudhary1975@gmail.com>
2020-12-26 18:00:46 +0530

    home screen , navigation drawer and splash screen added

--- app/src/main/java/com/riseinsteps/packbagbuddy_leisuretravelandtourism/MainActivity.java
+++ app/src/main/java/com/riseinsteps/packbagbuddy_leisuretravelandtourism/MainActivity.java
@@ -2,39 +2,13 @@ package com.riseinsteps.packbagbuddy_leisuretravelandtourism;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
-    ImageView ivSplash;
-    Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ivSplash = findViewById(R.id.ivSplash);
-        animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.a1);
-
-        ivSplash.startAnimation(animation);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                Intent i = new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(i);
-                finish();
-            }
-        }).start();
-
     }
 }
